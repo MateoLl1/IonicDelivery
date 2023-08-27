de2ec2127e9f9d8eefd2e13ceb4e9598faca02b4
Mateo <mateollerena40@gmail.com>
2023-08-26 23:14:08 -0500

    Conectando mi api en local (no valio 1.0)

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { BackendService } from 'src/app/service/backend.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  usuario: any[] = [];
+  error: any;
+  constructor(private apiMateo: BackendService, private http: HttpClient) {
+    // apiMateo.conectarApi().subscribe((data: any) => {
+    //   console.log(data);
+    //   this.usuario = data;
+    // });
+  }
+  ngOnInit(): void {
+    this.apiMateo.conectarApi().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.usuario = data;
+      },
+      (error: any) => {
+        console.error('Error en la solicitud:', error);
+        this.error = error; // Captura el error en la variable 'error'
+      }
+    );
+  }
 }
--- src/app/service/backend.service.ts
+++ src/app/service/backend.service.ts
@@ -6,7 +6,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class BackendService {
   ruta: string =
-    'BQD_EECryAasQJTOgjovsTF1XMG7remBOJbkJTbU3d2pMRjMfPGhhYwF5ftIeK_ZJum9G9ZvnPfLi9MFzV6jUFGNF2P22hS9o-wKMeJx5HSjzxuqDSg';
+    'BQDRHEXNysxyZWLNy6z4AmsbPnAEMuZKtfi_Al4l1WRZqsH5flwnXko-LvmKPC2aYC4NfoMQ8nB8qaa4yy3rrmOzMdYtuBIOUhQfjS8tLNVVLEnX4_U';
+
+  api: string = 'http://192.168.1.43:4040/Cargar';
   constructor(private http: HttpClient) {}
 
   getQuery(query: string) {
@@ -21,5 +23,9 @@ export class BackendService {
   conectarSpotify() {
     console.log('Conectado a Spotify');
     return this.getQuery('browse/new-releases');
+  }
+
+  conectarApi() {
+    return this.http.post(this.api, null);
   }
 }
